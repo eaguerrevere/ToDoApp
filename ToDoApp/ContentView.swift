3f2f2c68e928c5fa3b8abbd8d37427485287e746
Eli <elizabeth.aguerrevere@gmail.com>
2024-06-26 11:05:24 -0700

    Finished Iteration 0!!

--- ToDoApp/ContentView.swift
+++ ToDoApp/ContentView.swift
@@ -8,17 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showNewTask = false
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            
+            HStack {
+                Text("To Do List")
+                    .font(.system(size: 40))
+                    .fontWeight(.black)
+                Spacer()
+                
+                Button {
+                    withAnimation {
+                        self.showNewTask = true }
+                } label: {
+                    Text("+")
+                        .font(.system(size: 40))
+                }
+            } // closes HStack
+            .padding()
+            Spacer()
+            
+        } // closes VStack
+        
+        if showNewTask {
+            NewToDoView()
+        } // closes if statement
+        
+    } // closes body
+} // closes Struct
 
 #Preview {
     ContentView()
-}
+} // closes Preview
